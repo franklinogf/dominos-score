e12ca81a91f5ca07edc87314db10b764fcfcdf26
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-09-18 17:18:59 -0400

    feat: rename columns in database schema for consistency and add migration for changes

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,13 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_crazy_frightful_four.sql';
+import m0001 from './0001_clever_gambit.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001
     }
   }
   
